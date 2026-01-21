0568bd98a0e28e6966bc92cfe5c449cc99af0731
alike-krishna <alike.krishna@gmail.com>
2026-01-21 11:57:14 +0530

    github action CI

---
+++ tests/ui/actiondemo.spec.ts
@@ -0,0 +1,14 @@
+import { test, expect } from "@playwright/test";
+import { HomePage } from "@pages/HomePage";
+import { login } from "@utils/auth";
+
+test.describe("Home Page Smoke Tests after login", () => {
+  test("should login as standard_user and verify home page elements", async ({
+    page,
+  }) => {
+    await login(page);
+    // Home page assertions
+    const homePage = new HomePage(page);
+    await expect(homePage.homePageTitle).toBeVisible();
+  });
+});
